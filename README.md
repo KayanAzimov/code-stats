2ced58adcc57a6988efbe77d43bf6392ea5c5f4a
Kayan Azimov <kayan.aimov@hmcts.net>
2017-04-20 18:24:50 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,10 @@
+Just some scripts which could be usefull for different purposes
+
+**stats**
+Calculates number of line of code
+
+Usage:
+stats.sh $PRUJECT_PATH $LANGUAGE
+   
+Example:
+```stats.sh your_java_project_name java```   
---
+++ stats.sh
@@ -0,0 +1,25 @@
+if [ $# -lt 1 ]; then
+echo "usage: ./stats.sh project_name file_type[java, sh, js, yml, etc] [-V verbose] [-N no blank lines] "
+exit
+fi
+
+echo "calculating..."
+for line in $(find $1 -iname "*.$2" -print0 |  xargs -0 stat -f  "%z;%N" | sort -rn)
+do  
+    fileSize=$(echo $line | cut -d \; -f 1)
+    fileName=$(echo $line | cut -d \; -f 2)
+    
+	if [[ $@ == **-N** ]]; then 
+    	count=$(cat $fileName | sed '/^\s*$/d' | wc -l | awk '{ print $1 }')
+    else
+    	count=$(cat $fileName | wc -l | awk '{ print $1 }')	
+	fi
+    
+	(( total += count ))
+	
+	if [[ $@ == **-V** ]]; then 
+		echo "$fileName size: $fileSize lines: $count"
+	fi
+done
+echo "total lines: $total"
+
