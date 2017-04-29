0ead7ca6fce5cbf864c7d860b9d6d290ec487f94
Kayan Azimov <kayan.aimov@hmcts.net>
2017-04-29 08:40:14 +0100

    Improve memory stats

--- ru.sh
+++ ru.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-echo "cpu usage: $(ps -A -o %cpu | awk '{s+=$1} END {print s "%"}'), total: $(top -l 1  | grep 'CPU usage')"
+echo  "cpu usage total: $(echo "scale = 2; $(ps -A -o %cpu | awk '{s+=$1} END {print s ""}') / ("$(sysctl -n hw.ncpu)" * 100) * 100 "| bc ) %"
+
+echo "cpu usage: $(top -l 1  | grep 'CPU usage')"
 
 free -m
 
